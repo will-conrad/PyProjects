e9175b7b163ec046bdfb80377017998d210c236e
Will Conrad (Mac) <jwconrad23@gmail.com>
2021-06-20 15:46:30 -0400

    New

---
+++ PyShop/products/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('Hello World')
+
+def new(request):
+    return HttpResponse('New Products')
+
+
+
