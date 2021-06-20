4865cdb293314508ed1e7ff6cafcaf1fad7bcb05
Will Conrad (Mac) <jwconrad23@gmail.com>
2021-06-20 15:46:40 -0400

    New

---
+++ NeuralNet/cross_entropy.py
@@ -0,0 +1,9 @@
+import math
+softmax_output = [0.7, 0.1, 0.2]
+target_output = [1, 0, 0]
+loss = 0
+loss = -((math.log(softmax_output[0]) * target_output[0] +
+          math.log(softmax_output[1]) * target_output[1] +
+          math.log(softmax_output[2]) * target_output[2]))
+
+print(loss)
