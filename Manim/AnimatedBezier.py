819e4fa579f4306352277979cc7bc8f0826283a4
Will Conrad (Mac) <jwconrad23@gmail.com>
2022-01-28 22:49:07 -0500

    b e z i e r s

--- Manim/AnimatedBezier.py
+++ Manim/AnimatedBezier.py
@@ -1,16 +1,26 @@
 from manim import *
- 
-class AnimatedBezier(Scene):
-  def __init__(self, pts = [ORIGIN], colors=["#FFFFFF"]):
-    self.pts = pts
-    self.colors = colors
-  def create_beziers(self):
-    pts = np.array(self.pts)
-    colors = self.colors
+config.pixel_width = 1000
+config.pixel_height = 1000
+config.frame_height = 10
+config.frame_width = 10
+config.frame_rate = 120
+config.background_color = "#0E1B24"
+    
+class AnimatedBezierScene(Scene):
+  def construct(self):
+    pts = np.array([
+      [-3.5, -4, 0],
+      [-3.5, 1, 0],
+      [0, 3.5, 0],
+      [0, -3.5, 0],
+      [3.5, -1, 0],
+      [3.5, 4, 0]
+    ])
+    colors = ["#ef476f", "#ffd166", "#06d6a0", "#118ab2", "#118ab2"]
     t = ValueTracker(0)
-    points = VGroup(*[Dot(i, radius=0.1).set_fill(color=config.background_color).set_stroke(colors[0], 3, 1)for i in pts]) 
-    points[0].set_fill(colors[0])
-    lines = VGroup(*[Line(pts[i], pts[i+1]).set_stroke(color=colors[0],width=5, opacity=1)for i in range(len(pts)-1)])
+    points = VGroup(*[Dot(i, radius=0.1).set_fill(color=config.background_color).set_stroke(WHITE, 3, 1)for i in pts]) 
+    points[0].set_fill(color=WHITE)
+    lines = VGroup(*[Line(pts[i], pts[i+1]).set_stroke(width=5, opacity=1)for i in range(len(pts)-1)])
     
     def addSubLines(buffer, lines, c):  
       for i in range(buffer, len(lines)-1):
@@ -20,30 +30,24 @@ class AnimatedBezier(Scene):
         buffer+=1
       if len(lines) < (len(pts) - 1) * (len(pts) / 2):
         addSubLines(buffer + 1, lines, c+1)
-    # Recursively make and add lines
-    addSubLines(0, lines, 1)
+    
+    addSubLines(0, lines, 0)
     for i in range(len(lines) - 1):
-      d = Dot(lines[i].get_midpoint()).set_fill(color=colors[1+ int(i / (len(pts)-1))])
+      d = Dot(lines[i].get_midpoint()).set_fill(color=colors[int(i / (len(pts)-1))])
       points.add(d)
       d.add_updater(lambda mob, i=i : mob.move_to(lines[i].point_from_proportion(t.get_value())))
     
     tracer = Dot(points[0].get_center(), radius=0.1)
     tracer.add_updater(lambda mob : mob.move_to(lines[len(lines)-1].point_from_proportion(t.get_value())))
     points.add(tracer)
-    curve = VMobject().set_stroke(width=5)
+    curve = VMobject().set_stroke(width=7)
     curve.set_points_as_corners([points[0].get_center(), points[0].get_center()])
     def update_path(path):
         previous_path = path.copy()
         previous_path.add_points_as_corners([tracer.get_center()])
         path.become(previous_path)
     curve.add_updater(update_path)
-  def construct(self):
 
-
-
-class Main(AnimatedBezier):
-  def construct(self):
-    b = AnimatedBezier(pts=[[-2, -2, 0], [0, 3, 0], [3, -3, 0]])
-    self.create_bezier_lines()
-    self.add(lines, curve, points)
-    self.play(t.animate.set_value(1), run_time=3)
+    self.add(lines, points, curve)
+    self.play(t.animate.set_value(1), run_time=4)
+    self.wait()
