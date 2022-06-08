4e938cdc249f79cc2ac415e054c69716b96b206a
Will Conrad (PC) <jwconrad23@gmail.com>
2022-06-08 16:58:56 -0400

    things?

--- Manim/brachistochrone.py
+++ Manim/brachistochrone.py
@@ -1,9 +1,12 @@
+from cProfile import run
+from cairo import Gradient
 from manim import *
 
 config.frame_width = 16
 config.frame_height = 9
 config.pixel_width = 1280
 config.pixel_height = 720
+config.frame_rate = 60
 
 width = config.frame_width
 
@@ -15,8 +18,12 @@ class Main(Scene):
     p = ValueTracker(0)
     self.add(floor)
 
-    roller = Circle(1.5).shift(DOWN*0.5 + LEFT * 6).set
+    roller = Circle(1.5).shift(DOWN*0.5 + LEFT * 6).set_fill(color=color_gradient([RED, BLUE], length_of_output=100), opacity=1)
+    roller.add_updater(
+      lambda mob : mob.move_to(floor.point_from_proportion(p.get_value())).shift(UP * 1.5).rotate(-0.1)
+      )
+    
     self.add(roller)
     
 
-    self.wait()
+    self.play(p.animate(rate_func=linear).set_value(1), run_time=3)
